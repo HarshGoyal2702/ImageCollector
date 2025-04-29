9233118a673dddf1c135b8302d4829c33944e2bd
HarshGoyal2702 <harshgoyal1331@gmail.com>
2025-04-29 22:51:38 +0530

    image collector for the event

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+# MONGO_URI = "mongodb+srv://harshgoyal1331:HarshGoyal2702@imageupload.es3npuc.mongodb.net/"
+# MONGO_URI = "mongodb+srv://harshgoyal1331:HarshGoyal2702@imageupload.es3npuc.mongodb.net/?retryWrites=true&w=majority&appName=ImageUpload"
+MONGO_URI = "mongodb+srv://harshgoyal1331:HarshGoyal2702@imageupload.es3npuc.mongodb.net/imageupload?retryWrites=true&w=majority"
+
+SECRET_KEY = "supersecretkey123"
+UPLOAD_FOLDER = "static/uploads"
+RESULT_FOLDER = "static/results"
+ZIP_FOLDER = "static/zips"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
